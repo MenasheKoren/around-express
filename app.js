f39c797d996dfbfd6961fc4c95a18a0cdae6d81a
menashekoren <menashekoren@gmail.com>
2021-12-20 20:50:30 +0200

    Feat: Build route to full user json list

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const users = require("./routes/users");
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(users);
-app.get('/', (req, res) => {
-  res.status(200).send('<h1>Home page</h1>')
-})
+app.use('/users', users);
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
-const { users } = require("../data/users.json");
+const users = require("../data/users.json");
 
-router.get("/users", (req, res) => {
-  res.send({ users });
+router.use(function (req, res, next) {
+  console.log('Keep going');
+
+  next();
 });
+
+router.get("/", (req, res) => {
+  res.status(200).send(users);
+});
+
+
 
 module.exports = router;
