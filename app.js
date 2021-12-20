528c71ebc83c64de43ccbb76c6c72127de304ff2
menashekoren <menashekoren@gmail.com>
2021-12-20 11:11:40 +0200

    Feat: WIP query users.js

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const users = require("./data/users.json");
 const { PORT = 3000 } = process.env;
 const app = express();
-
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
-});
-
-app.get('/users', (req, res) => {
-  res.send(req.query);
-});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-module.exports = app;
+
+app.get("/users", (req, res) => {
+  res.send(req.query);
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
