6c039fa889a054339b33a3a329796f2bc7fdb4d6
menashekoren <menashekoren@gmail.com>
2021-12-20 10:38:10 +0200

    Feat: Remove jest tests

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const users = require("./data/users.json");
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
+});
+
+app.get('/users', (req, res) => {
+  res.send(req.query);
 });
 
 app.listen(PORT, () => {
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
 const { users } = require('../data/users.json');
 
-router.get('/users/:id', (req, res) => {
-  if (!users[req.params.id]) {
-    const error = { error: `This user doesn't exist` }
-    res.send({error});
+router.get('/users', (req, res) => {
+  res.send(users);
+});
 
+router.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!users[id]) {
+    res.send({ error: `This user doesn't exist` });
     return;
   }
-  res.send(users[req.params.id]);
+
+  res.send(users[id]);
 });
+
 
 module.exports = router;
