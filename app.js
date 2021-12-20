b7ec6493fe12439447df3f2dd227d2083dd6b2ab
menashekoren <menashekoren@gmail.com>
2021-12-20 17:16:16 +0200

    Feat: WIP update get users json

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const express = require("express");
+const users = require("./routes/users");
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(users);
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Home page</h1>')
+})
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-});
-app.use(express.json());
-
-app.use(function (req, res, next) {
-  console.log(req.body);
-  next();
-});
-
-app.post("/users/", (req, res) => {
-  res.send(req.query);
-});
-
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
 });
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,8 @@
-const router = require('express').Router();
-const { users } = require('../data/users.json');
+const router = require("express").Router();
+const { users } = require("../data/users.json");
 
-router.get('/users', (req, res) => {
-  res.send(users);
+router.get("/users", (req, res) => {
+  res.send({ users });
 });
-
-router.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-
-  if (!users[id]) {
-    res.send({ error: `This user doesn't exist` });
-    return;
-  }
-
-  res.send(users[id]);
-});
-
 
 module.exports = router;
