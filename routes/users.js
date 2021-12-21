690eae6705775cbdd963f6d50c0cff3e81228b67
menashekoren <menashekoren@gmail.com>
2021-12-21 09:03:35 +0200

    Feat: WIP check for correct ID

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,23 @@
 const router = require("express").Router();
 const users = require("../data/users.json");
 
-router.use(function (req, res, next) {
-  console.log('Keep going');
+const doesUserExist = (req, res, next) => {
+  if (!users[req.params.id]) {
+    res.send(`This user doesn't exist`);
+    return;
+  }
 
   next();
-});
+};
+
+const sendUser = (req, res) => {
+  res.send(users[req.params.id]);
+};
 
 router.get("/", (req, res) => {
   res.status(200).send(users);
 });
 
-
+router.get("/:id", doesUserExist, sendUser);
 
 module.exports = router;
