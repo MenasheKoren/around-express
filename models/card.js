a44d2411435bd9b08f5f54440e63a8c65087cc76
menashekoren <menashekoren@gmail.com>
2022-01-03 14:00:36 +0200

    feat: WIP create card schema

---
+++ models/card.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  link: {
+    type: String,
+    validate: {
+      validator(v) {
+        return /https?:\/{2}(?:(\w+\W(com)?)\/?)+\1?#?/gi.test(v);
+      },
+      message: (props) => `${props.value} is not a valid URL!`,
+    },
+    required: true,
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+});
--- models/user.js
+++ models/user.js
@@ -24,3 +24,5 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+module.exports = mongoose.model('user', userSchema);
