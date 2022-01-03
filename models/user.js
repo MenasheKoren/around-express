ad1b18a3535ee6bf2627509c34f58cb84134f629
menashekoren <menashekoren@gmail.com>
2022-01-03 13:15:00 +0200

    feat: Create user schema

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  avatar: {
+    type: String,
+    validate: {
+      validator(v) {
+        return /https?:\/{2}(?:(\w+\W(com)?)\/?)+\1?#?/gi.test(v);
+      },
+      message: (props) => `${props.value} is not a valid URL!`,
+    },
+    required: true,
+  },
+});
